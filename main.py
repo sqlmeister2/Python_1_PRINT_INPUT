cde9cf0e4ef8597ac8ffc81ba785e79ff937b8ef
Mario2 <94698816+mariopl2020@users.noreply.github.com>
2023-09-29 22:29:09 +0200

    Instrukcja PRINT INPUT

---
+++ main.py
@@ -0,0 +1,28 @@
+
+# drukowanie na konsole wartosci
+print(5 + 7)
+print(10 - 7)
+print(12 / 7)
+print(4 * 3)
+print(13 % 3)
+print("Hello")
+print("World "*5)
+
+#brak typow danych przy zapisie
+print('\n')
+zmienna1 = 5
+print(zmienna1)
+zmienna1 = "Mario"
+print(zmienna1)
+
+#pobieranie wartosci z konsoli (jak skaner w javie)
+#zmienna2 = input()
+#print(zmienna2)
+zmienna3 = input("Podaj imie \n")
+print(zmienna3)
+zmienna4 = "Hello"
+print(zmienna4 + " " + zmienna3)
+
+
+
+
